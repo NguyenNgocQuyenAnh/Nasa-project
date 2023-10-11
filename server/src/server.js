62c132f801290cc51a91d7d140f14972c4cc8669
Nguyen Ngoc Quyen Anh <nnquyenanh@gmail.com>
2023-10-11 21:12:25 +0700

    first commit

---
+++ server/src/server.js
@@ -0,0 +1,13 @@
+const http = require('http');
+const PORT = process.env.PORT || 8000;
+const app = require('./app');
+const {loadPlanetsData} = require('./models/planets.model');
+const server = http.createServer(app);
+
+async function startServer(){
+    await loadPlanetsData();
+    server.listen(PORT, ()=>{
+        console.log(`listening on port ${PORT}`);
+    });
+}
+startServer();
